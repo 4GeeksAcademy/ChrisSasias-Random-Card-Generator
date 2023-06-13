a4b817c23dd97fbc9a92776291bb3638a63be329
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-06-13 22:26:34 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardSuits = ["♦", "♣", "♠", "♥"];
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, "J", "K", "Q", "A"];
+
+  function generateRandom(arr) {
+    let random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+  }
+
+  document.getElementById("values").innerHTML = generateRandom(values);
+
+  const randomSuits = generateRandom(cardSuits);
+
+  for (let suit of ["top", "bot"]) {
+    suit = document.getElementById(suit);
+
+    suit.style.color =
+      randomSuits === "♥" || randomSuits === "♦" ? "red" : "black";
+
+    suit.innerHTML = randomSuits;
+  }
 };
+setInterval(window.onload, 3400);
